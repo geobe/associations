0479bc3ae480c3c9517bf57ef75087d8a959c5b4
geobe <code@georgbeier.de>
2018-01-12 16:32:05 +0100

    completed spock tests for associations

--- src/test/groovy/de/geobe/util/association/AssociationSpecification.groovy
+++ src/test/groovy/de/geobe/util/association/AssociationSpecification.groovy
@@ -24,8 +24,7 @@
 
 package de.geobe.util.association
 
-import de.geobe.util.association.test.One1
-import de.geobe.util.association.test.One2
+import de.geobe.util.association.test.*
 import groovy.util.logging.Slf4j
 import spock.lang.Specification
 
@@ -40,15 +39,15 @@ class AssociationSpecification extends Specification {
         One1 a = new One1()
         One2 b1 = new One2()
         One2 b2 = new One2()
-        when: 'b1 is associated vith a'
+        when: 'b1 is associated with a'
         b1.one1bi.add a
         then: 'both objects are associated'
         assert a.one2bi.one == b1
         assert a.one2bi.all[0] == b1
         assert b1.one1bi.one == a
-        when: 'b2 is associated vith a'
+        when: 'b2 is associated with a'
         b2.one1bi.add a
-        then: 'both objects are associatedand old association is released'
+        then: 'both objects are associated and old association is released'
         assert a.one2bi.one == b2
         assert b1.one1bi.one == null
         when: 'b2 is removed from a'
@@ -67,21 +66,27 @@ class AssociationSpecification extends Specification {
         then: 'association is empty'
         assert b1.one1bi.one == null
         assert a.one2bi.one == null
+        when: 'two objects are added to aas array'
+        a.one2bi.add([b1, b2].toArray())
+        then: 'only the first should be added'
+        assert a.one2bi.one == b1
+        assert b1.one1bi.one == a
+        assert b2.one1bi.one == null
     }
 
-    def 'unidirectional 1-1 associations should work'() {
+    def 'unidirectional to-1 associations should work'() {
         setup: 'a couple of classes'
         One1 a = new One1()
         One2 b1 = new One2()
         One2 b2 = new One2()
-        when: 'b1 is associated vith a'
+        when: 'b1 is associated with a'
         a.one2uni.add b1
         then: 'both objects are associated'
         assert a.one2uni.one == b1
         assert a.one2uni.all[0] == b1
-        when: 'b2 is associated vith a'
+        when: 'b2 is associated with a'
         a.one2uni.add b2
-        then: 'both objects are associatedand old association is released'
+        then: 'both objects are associated and old association is released'
         assert a.one2uni.one == b2
         when: 'b2 is removed from a'
         a.one2uni.remove b2
@@ -95,5 +100,130 @@ class AssociationSpecification extends Specification {
         a.one2uni.removeAll()
         then: 'association is empty'
         assert a.one2uni.one == null
+    }
+
+    def 'bidirectional N-1 associations should work'() {
+        setup: 'a couple of classes'
+        One1 a = new One1()
+        Many2 b1 = new Many2()
+        Many2 b2 = new Many2()
+        Many2 b3 = new Many2()
+        when: 'b1 and b2 are associated with a'
+        b1.one1bi.add a
+        a.many2bi.add b2
+        then: 'both objects are associated'
+        assert [b1, b2].contains(a.many2bi.one)
+        assert a.many2bi.all.containsAll([b1, b2])
+        assert b1.one1bi.one == a
+        when: 'b2 is removed from a'
+        a.many2bi.remove b2
+        then:
+        assert b2.one1bi.one == null
+        assert a.many2bi.one == b1
+        when: 'all associated objects are removed'
+        a.many2bi.removeAll()
+        then: 'association is empty'
+        assert b1.one1bi.one == null
+        assert a.many2bi.one == null
+        assert a.many2bi.all.size() == 0
+        when: 'three objects are associated with a'
+        a.many2bi.add([b1, b2, b3])
+        then: 'only the first should be added'
+        assert a.many2bi.all.size() == 3
+        assert b1.one1bi.one == a
+        assert b2.one1bi.one == a
+        assert b3.one1bi.one == a
+    }
+
+    def 'unidirectional to-M associations should work'() {
+        setup: 'a couple of classes'
+        Many1 a = new Many1()
+        Any b1 = new Any()
+        Any b2 = new Any()
+        when: 'b1 is associated with a'
+        a.anybi.add b1
+        then: 'both objects are associated'
+        assert a.anybi.one == b1
+        when: 'b2 is also associated with a'
+        a.anybi.add b2
+        then: 'both objects are associated '
+        assert a.anybi.all.containsAll([b1, b2])
+        when: 'b1 is removed from a'
+        a.anybi.remove b1
+        then:
+        assert a.anybi.one == b2
+        when: 'two objects are associated with a'
+        a.anybi.add([b1, b2])
+        then: 'both should be added'
+        assert a.anybi.all.containsAll([b1, b2])
+        when: 'all associated objects are removed'
+        a.anybi.removeAll()
+        then: 'association is empty'
+        assert a.anybi.one == null
+        assert a.anybi.all.size() == 0
+    }
+
+    def 'bidirectional M-N associations should work'() {
+        setup: 'a couple of classes'
+        Many1 a1 = new Many1()
+        Many1 a2 = new Many1()
+        Many1 a3 = new Many1()
+        Many2 b1 = new Many2()
+        Many2 b2 = new Many2()
+        Many2 b3 = new Many2()
+        when: 'b1 and b2 are associated with a'
+        a1.many2bi.add b1
+        a1.many2bi.add b2
+        a1.many2bi.add b1
+        a1.many2bi.add b2
+        a1.many2bi.add b3
+        a2.many2bi.add b1
+        a2.many2bi.add b2
+        a3.many2bi.add b1
+        then: 'all objects are associated'
+        assert [b1, b2, b3].contains(a1.many2bi.one)
+        assert a1.many2bi.all.containsAll([b1, b2, b3])
+        assert a1.many2bi.all.size() == 3
+        assert b1.many1bi.all.containsAll([a1, a2, a3])
+        assert b3.many1bi.one == a1
+        when: 'b2 is removed from a1'
+        a1.many2bi.remove b2
+        then:
+        assert !b2.many1bi.all.contains(a1)
+        assert !a1.many2bi.all.contains(b2)
+        assert a1.many2bi.all.containsAll([b1, b3])
+        assert b2.many1bi.all.contains(a2)
+        assert b1.many1bi.all.contains(a1)
+        when: 'all associated objects are removed'
+        a1.many2bi.removeAll()
+        then: 'association is emptied on both sides'
+        assert b1.many1bi.all.containsAll([a2, a3])
+        assert !b1.many1bi.all.contains(a1)
+        assert a1.many2bi.one == null
+        assert a1.many2bi.all.size() == 0
+        when: 'three objects are associated with a1'
+        b1.many1bi.removeAll()
+        b2.many1bi.removeAll()
+        b3.many1bi.removeAll()
+        a1.many2bi.add([b1, b2, b3, b3, b3, b1, b2])
+        then: 'all should be added exactly once'
+        assert a1.many2bi.all.containsAll([b1, b2, b3])
+        assert a1.many2bi.all.size() == 3
+        assert b1.many1bi.one == a1
+        assert b1.many1bi.all.size() == 1
+        assert b2.many1bi.one == a1
+        assert b3.many1bi.one == a1
+        when: 'three objects are added as array to a1'
+        b1.many1bi.removeAll()
+        b2.many1bi.removeAll()
+        b3.many1bi.removeAll()
+        a1.many2bi.add([b1, b2, b3, b3, b3, b1, b2].toArray())
+        then: 'all should be added exactly once'
+        assert a1.many2bi.all.containsAll([b1, b2, b3])
+        assert a1.many2bi.all.size() == 3
+        assert b1.many1bi.one == a1
+        assert b1.many1bi.all.size() == 1
+        assert b2.many1bi.one == a1
+        assert b3.many1bi.one == a1
     }
 }
--- src/test/groovy/de/geobe/util/association/test/TestClass.groovy
+++ src/test/groovy/de/geobe/util/association/test/TestClass.groovy
@@ -26,6 +26,7 @@ package de.geobe.util.association.test
 
 import de.geobe.util.association.IGetOther
 import de.geobe.util.association.IToAny
+import de.geobe.util.association.ToMany
 import de.geobe.util.association.ToOne
 
 /**
@@ -33,7 +34,7 @@ import de.geobe.util.association.ToOne
  */
 
 /**
- * Target for to-one associations
+ * Origin of 1-X associations
  */
 class One1 {
 
@@ -45,18 +46,26 @@ class One1 {
             this,
             { One2 o2 -> o2.one1bi } as IGetOther
     )
-
     IToAny<One1> getOne2bi() { toOne2bi }
 
-    /** bidirectional 1-1 association */
+    /** unidirectional 1-1 association */
     private One2 one2uni
     private ToOne<One1, One2> toOne2uni = new ToOne<>(
             { this.@one2uni } as IToAny.IGet,
             { One2 o2 -> this.@one2uni = o2 } as IToAny.ISet,
             this, null
     )
-
     IToAny<One1> getOne2uni() { toOne2uni }
+
+    /** bidirectional 1-N association */
+    private List<Many2> many2bi = new ArrayList<>()
+    private ToMany<One1, Many2> toMany2bi = new ToMany<>(
+            { this.@many2bi } as IToAny.IGet,
+            this,
+            { Many2 m2 -> m2.one1bi } as IGetOther
+    )
+
+    IToAny<One1> getMany2bi() { toMany2bi }
 }
 
 /**
@@ -74,5 +83,64 @@ class One2 {
     )
 
     IToAny<One1> getOne1bi() { toOne1bi }
+}
+
+/**
+ * Target for unidirectional to-N associations
+ */
+class Any {
+    def any = 'Any get your gun ;)'
+}
+
+/**
+ * Origin of to-Many associations
+ */
+class Many1 {
+
+    /** unidirectional to-N association */
+    private List<Any> many2uni = new ArrayList<>()
+    private ToMany<Many1, Any> toAnyUni = new ToMany<>(
+            { this.@many2uni } as IToAny.IGet,
+            this, null
+    )
+
+    IToAny<Many1> getAnybi() { toAnyUni }
+
+    /** bidirectional M-N association */
+    private List<Many2> many2bi = new ArrayList<>()
+    private ToMany<Many1, Many2> toMany2bi = new ToMany<>(
+            { this.@many2bi } as IToAny.IGet,
+            this,
+            { Many2 m2 -> m2.many1bi } as IGetOther
+    )
+
+    IToAny<One1> getMany2bi() { toMany2bi }
+}
+
+/**
+ * Target for to-Many associations
+ */
+class Many2 {
+
+    /** bidirectional N-1 association */
+    private One1 one1bi
+    private ToOne<Many2, One1> toOne1bi = new ToOne<>(
+            { this.@one1bi } as IToAny.IGet,
+            { One1 o1 -> this.@one1bi = o1 } as IToAny.ISet,
+            this,
+            { One1 o1 -> o1.many2bi } as IGetOther
+    )
+
+    IToAny<One1> getOne1bi() { toOne1bi }
+
+    /** bidirectional M-N association */
+    private List<Many1> many1bi = new ArrayList<>()
+    private ToMany<Many2, Many2> toMany1bi = new ToMany<>(
+            { this.@many1bi } as IToAny.IGet,
+            this,
+            { Many1 m1 -> m1.many2bi } as IGetOther
+    )
+
+    IToAny<Many1> getMany1bi() { toMany1bi }
 }
 
